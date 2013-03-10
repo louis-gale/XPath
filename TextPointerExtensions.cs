e674be06d8fe8cbb972bdd206281b1e4250488ae
Xcalibur_cp <SND\Xcalibur_cp>
2013-03-10 12:20:06 +0000

    - Optimised text selection to be significantly faster

--- TextPointerExtensions.cs
+++ TextPointerExtensions.cs
@@ -6,25 +6,7 @@ namespace Inverse.XPath
     {
         internal static TextPointer GetOffsetTextPointer(this TextPointer start, int offset)
         {
-            var ret = start;
-            var i = 0;
-
-            while (i < offset && ret != null)
-            {
-                if (ret.GetPointerContext(LogicalDirection.Backward) == TextPointerContext.Text || ret.GetPointerContext(LogicalDirection.Backward) == TextPointerContext.None)
-                {
-                    i++;
-                }
-
-                if (ret.GetPositionAtOffset(1, LogicalDirection.Forward) == null)
-                {
-                    return ret;
-                }
-
-                ret = ret.GetPositionAtOffset(1, LogicalDirection.Forward);
-            }
-
-            return ret;
+            return start.GetInsertionPosition(LogicalDirection.Forward).GetPositionAtOffset(offset);
         }
     }
 }
